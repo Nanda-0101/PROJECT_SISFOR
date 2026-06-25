1a62903bd0553d594e71afba25372786142b5281
Hesa Dharma <hesamade@gmail.com>
2026-06-25 12:05:20 +0800

    Update Dash Panitia & Login Panit

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ Route::get('/panitia-login', function () {
     return view('panitia.loginpage_panitia'); 
 });
 
+Route::get('/panitia-dashboard', function () {
+    return view('panitia.dashboard_panitia');
+});
+
+Route::get('/panitia-data-peserta', function () {
+    return view('panitia.dataPeserta_panitia');
+});
+
+Route::get('/panitia-profil', function () {
+    return view('panitia.profil_panitia');
+});
+
+Route::get('/panitia-tutup-sesi', function () {
+    return view('panitia.tutupSesi_panitia');
+});
+
 Route::get('/public-landingpage', function () {
     return view('public.landingpage_public'); 
 });
